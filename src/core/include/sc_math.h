b7d50debf590d28774d95e89b7bf1c9fab915206
Skeletus <Samol_88@hotmail.com>
2026-02-07 18:02:44 -0500

    improvec mat4

--- src/core/include/sc_math.h
+++ src/core/include/sc_math.h
@@ -3,22 +3,57 @@
 
 namespace sc
 {
-  struct Mat4
+  inline constexpr float EPSILON = 1e-6f;
+
+  struct alignas(16) Mat4
   {
     alignas(16) float m[16]{};
 
-    static Mat4 identity();
+    [[nodiscard]] static constexpr Mat4 identity() noexcept
+    {
+      Mat4 r{};
+      r.m[0] = 1.0f;
+      r.m[5] = 1.0f;
+      r.m[10] = 1.0f;
+      r.m[15] = 1.0f;
+      return r;
+    }
   };
 
-  Mat4 mat4_identity();
-  Mat4 mat4_mul(const Mat4& a, const Mat4& b);
-  Mat4 mat4_transpose(const Mat4& a);
-  Mat4 mat4_translation(float x, float y, float z);
-  Mat4 mat4_scale(float x, float y, float z);
-  Mat4 mat4_rotation_xyz(float rx, float ry, float rz);
-  Mat4 mat4_trs(const float pos[3], const float rot[3], const float scale[3]);
-  Mat4 mat4_inverse(const Mat4& a);
+  static_assert(sizeof(Mat4) == sizeof(float) * 16, "Mat4 must be 16 floats.");
+  static_assert(alignof(Mat4) >= 16, "Mat4 must be at least 16-byte aligned.");
+
+  [[nodiscard]] inline constexpr Mat4 mat4_identity() noexcept
+  {
+    return Mat4::identity();
+  }
+
+  [[nodiscard]] Mat4 mat4_mul(const Mat4& a, const Mat4& b) noexcept;
+  [[nodiscard]] Mat4 mat4_transpose(const Mat4& a) noexcept;
+
+  [[nodiscard]] inline constexpr Mat4 mat4_translation(float x, float y, float z) noexcept
+  {
+    Mat4 r = Mat4::identity();
+    r.m[12] = x;
+    r.m[13] = y;
+    r.m[14] = z;
+    return r;
+  }
+
+  [[nodiscard]] inline constexpr Mat4 mat4_scale(float x, float y, float z) noexcept
+  {
+    Mat4 r{};
+    r.m[0] = x;
+    r.m[5] = y;
+    r.m[10] = z;
+    r.m[15] = 1.0f;
+    return r;
+  }
+
+  [[nodiscard]] Mat4 mat4_rotation_xyz(float rx, float ry, float rz) noexcept;
+  [[nodiscard]] Mat4 mat4_trs(const float pos[3], const float rot[3], const float scale[3]) noexcept;
+  [[nodiscard]] Mat4 mat4_inverse(const Mat4& a) noexcept;
 
   // Right-handed, depth 0..1 (Vulkan). Set flipY=true to keep +Y up in world.
-  Mat4 mat4_perspective_rh_zo(float fovYRadians, float aspect, float zNear, float zFar, bool flipY);
+  [[nodiscard]] Mat4 mat4_perspective_rh_zo(float fovYRadians, float aspect, float zNear, float zFar, bool flipY) noexcept;
 }
--- src/core/src/sc_math.cpp
+++ src/core/src/sc_math.cpp
@@ -1,25 +1,72 @@
 #include "sc_math.h"
+#include <cassert>
 #include <cmath>
+
+#if defined(__AVX__) || defined(__SSE__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
+#include <immintrin.h>
+#endif
 
 namespace sc
 {
-  Mat4 Mat4::identity()
+  Mat4 mat4_mul(const Mat4& a, const Mat4& b) noexcept
   {
+#if defined(__AVX__)
     Mat4 r{};
-    r.m[0] = 1.0f;
-    r.m[5] = 1.0f;
-    r.m[10] = 1.0f;
-    r.m[15] = 1.0f;
+    const float* bm = b.m;
+
+    const __m128 a0 = _mm_loadu_ps(a.m + 0);
+    const __m128 a1 = _mm_loadu_ps(a.m + 4);
+    const __m128 a2 = _mm_loadu_ps(a.m + 8);
+    const __m128 a3 = _mm_loadu_ps(a.m + 12);
+
+    const __m256 a0_2 = _mm256_set_m128(a0, a0);
+    const __m256 a1_2 = _mm256_set_m128(a1, a1);
+    const __m256 a2_2 = _mm256_set_m128(a2, a2);
+    const __m256 a3_2 = _mm256_set_m128(a3, a3);
+
+    auto mul_pair = [&](int col0, int col1)
+    {
+      const int b0 = col0 * 4;
+      const int b1 = col1 * 4;
+
+      __m256 res = _mm256_mul_ps(a0_2, _mm256_set_ps(
+        bm[b1 + 0], bm[b1 + 0], bm[b1 + 0], bm[b1 + 0],
+        bm[b0 + 0], bm[b0 + 0], bm[b0 + 0], bm[b0 + 0]));
+      res = _mm256_add_ps(res, _mm256_mul_ps(a1_2, _mm256_set_ps(
+        bm[b1 + 1], bm[b1 + 1], bm[b1 + 1], bm[b1 + 1],
+        bm[b0 + 1], bm[b0 + 1], bm[b0 + 1], bm[b0 + 1])));
+      res = _mm256_add_ps(res, _mm256_mul_ps(a2_2, _mm256_set_ps(
+        bm[b1 + 2], bm[b1 + 2], bm[b1 + 2], bm[b1 + 2],
+        bm[b0 + 2], bm[b0 + 2], bm[b0 + 2], bm[b0 + 2])));
+      res = _mm256_add_ps(res, _mm256_mul_ps(a3_2, _mm256_set_ps(
+        bm[b1 + 3], bm[b1 + 3], bm[b1 + 3], bm[b1 + 3],
+        bm[b0 + 3], bm[b0 + 3], bm[b0 + 3], bm[b0 + 3])));
+
+      _mm_storeu_ps(r.m + b0, _mm256_castps256_ps128(res));
+      _mm_storeu_ps(r.m + b1, _mm256_extractf128_ps(res, 1));
+    };
+
+    mul_pair(0, 1);
+    mul_pair(2, 3);
     return r;
-  }
+#elif defined(__SSE__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
+    Mat4 r{};
+    const __m128 a0 = _mm_loadu_ps(a.m + 0);
+    const __m128 a1 = _mm_loadu_ps(a.m + 4);
+    const __m128 a2 = _mm_loadu_ps(a.m + 8);
+    const __m128 a3 = _mm_loadu_ps(a.m + 12);
 
-  Mat4 mat4_identity()
-  {
-    return Mat4::identity();
-  }
-
-  Mat4 mat4_mul(const Mat4& a, const Mat4& b)
-  {
+    for (int col = 0; col < 4; ++col)
+    {
+      const float* bc = b.m + col * 4;
+      __m128 res = _mm_mul_ps(a0, _mm_set1_ps(bc[0]));
+      res = _mm_add_ps(res, _mm_mul_ps(a1, _mm_set1_ps(bc[1])));
+      res = _mm_add_ps(res, _mm_mul_ps(a2, _mm_set1_ps(bc[2])));
+      res = _mm_add_ps(res, _mm_mul_ps(a3, _mm_set1_ps(bc[3])));
+      _mm_storeu_ps(r.m + col * 4, res);
+    }
+    return r;
+#else
     Mat4 r{};
     for (int col = 0; col < 4; ++col)
     {
@@ -34,9 +81,10 @@ namespace sc
       }
     }
     return r;
+#endif
   }
 
-  Mat4 mat4_transpose(const Mat4& a)
+  Mat4 mat4_transpose(const Mat4& a) noexcept
   {
     Mat4 r{};
     for (int col = 0; col < 4; ++col)
@@ -49,26 +97,7 @@ namespace sc
     return r;
   }
 
-  Mat4 mat4_translation(float x, float y, float z)
-  {
-    Mat4 r = Mat4::identity();
-    r.m[12] = x;
-    r.m[13] = y;
-    r.m[14] = z;
-    return r;
-  }
-
-  Mat4 mat4_scale(float x, float y, float z)
-  {
-    Mat4 r{};
-    r.m[0] = x;
-    r.m[5] = y;
-    r.m[10] = z;
-    r.m[15] = 1.0f;
-    return r;
-  }
-
-  Mat4 mat4_rotation_xyz(float rx, float ry, float rz)
+  Mat4 mat4_rotation_xyz(float rx, float ry, float rz) noexcept
   {
     const float cx = std::cos(rx);
     const float sx = std::sin(rx);
@@ -98,15 +127,21 @@ namespace sc
     return mat4_mul(mat4_mul(rzm, rym), rxm);
   }
 
-  Mat4 mat4_trs(const float pos[3], const float rot[3], const float scale[3])
+  Mat4 mat4_trs(const float pos[3], const float rot[3], const float scale[3]) noexcept
   {
+    assert(pos != nullptr);
+    assert(rot != nullptr);
+    assert(scale != nullptr);
+    if (!pos || !rot || !scale)
+      return Mat4::identity();
+
     const Mat4 t = mat4_translation(pos[0], pos[1], pos[2]);
     const Mat4 r = mat4_rotation_xyz(rot[0], rot[1], rot[2]);
     const Mat4 s = mat4_scale(scale[0], scale[1], scale[2]);
     return mat4_mul(t, mat4_mul(r, s));
   }
 
-  Mat4 mat4_inverse(const Mat4& a)
+  Mat4 mat4_inverse(const Mat4& a) noexcept
   {
     const float* m = a.m;
     Mat4 inv{};
@@ -160,19 +195,26 @@ namespace sc
     o[15] = m[0] * m[5] * m[10] - m[0] * m[6] * m[9] - m[4] * m[1] * m[10]
           + m[4] * m[2] * m[9] + m[8] * m[1] * m[6] - m[8] * m[2] * m[5];
 
-    float det = m[0] * o[0] + m[1] * o[4] + m[2] * o[8] + m[3] * o[12];
-    if (det == 0.0f)
+    const float det = m[0] * o[0] + m[1] * o[4] + m[2] * o[8] + m[3] * o[12];
+    if (std::fabs(det) <= EPSILON)
       return Mat4::identity();
 
-    det = 1.0f / det;
+    const float inv_det = 1.0f / det;
     for (int i = 0; i < 16; ++i)
-      o[i] *= det;
+      o[i] *= inv_det;
 
     return inv;
   }
 
-  Mat4 mat4_perspective_rh_zo(float fovYRadians, float aspect, float zNear, float zFar, bool flipY)
+  Mat4 mat4_perspective_rh_zo(float fovYRadians, float aspect, float zNear, float zFar, bool flipY) noexcept
   {
+    assert(fovYRadians > 0.0f);
+    assert(aspect > 0.0f);
+    assert(zNear > 0.0f);
+    assert(zFar > zNear);
+    if (fovYRadians <= EPSILON || aspect <= EPSILON || zNear <= EPSILON || zFar <= zNear + EPSILON)
+      return Mat4::identity();
+
     Mat4 r{};
     const float f = 1.0f / std::tan(fovYRadians * 0.5f);
 
